91e8b8e5b5c8073b4388174bcb1c94b0dbecbce9
Osaro Imohe <osaroimohe99@yahoo.com>
2022-09-20 09:29:32 +0100

    Add padding to art tab component

--- components/arttab/index.tsx
+++ components/arttab/index.tsx
@@ -17,10 +17,12 @@ const ArtTab = ({ art }: ArtProps) => (
 	<div className="sm:w-12/12 md:w-6/12 lg:w-4/12 px-2 py-2">
 		<div className="w-full h-96 border-solid border-2 border-zinc-100 rounded-md overflow-hidden">
 			<Image src={art.image_url} alt={art.title} width="100%" height="50%" layout="responsive" objectFit="cover" />
-			<p className="text-purple-600 font-normal">{art.title}</p>
-			<p className="mt-2 text-zinc-500 font-light">{art.artist} - {art.created_at}</p>
-			<p className="mt-2 text-zinc-500 font-thin">{art.medium}</p>
-			<p className="mt-2 text-zinc-500 font-thin">{art.description}</p> 
+			<div className="px-2 w-full">
+				<p className="text-purple-600 font-normal">{art.title}</p>
+				<p className="mt-2 text-zinc-500 font-light">{art.artist} - {art.created_at}</p>
+				<p className="mt-2 text-zinc-500 font-thin">{art.medium}</p>
+				<p className="mt-2 text-zinc-500 font-thin">{art.description}</p> 
+			</div>
 			<p></p>
 		</div>
 	</div>
